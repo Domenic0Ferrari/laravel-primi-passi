8fab4f045374f8765c186b5f3eefd4ecf1bb85e2
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-06-22 17:18:59 +0200

    add function in pricing.blade.php

--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -18,7 +18,22 @@
         <button type="button" class="btn btn-primary">
             <a class="nav-link" href="http://127.0.0.1:8000/">Torna alla pagina Welcome</a>
         </button>
+        <div>
+            <p id="my-text">Ciao a tutti!</p>
+            <button type="button" onclick="changeColor('red')" class="btn btn-primary">Cambia il colore in Rosso!</button>
+            <button type="button" onclick="changeColor('green')" class="btn btn-primary">Cambia il colore in Verde!</button>
+            <button type="button" onclick="changeColor('black')" class="btn btn-primary">Cambia il colore in Nero!</button>
+        </div>
     </div>
 </body>
 
+
 </html>
+
+<script>
+    const myText = document.querySelector("#my-text");
+
+    function changeColor(color) {
+        myText.style.color = color;
+    }
+</script>
