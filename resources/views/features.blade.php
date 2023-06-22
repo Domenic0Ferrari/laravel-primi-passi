b82c83decba4b27cfd4c4b90760d44492fd4ba1d
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-06-22 17:32:03 +0200

    add function in features.blade.php

--- resources/views/features.blade.php
+++ resources/views/features.blade.php
@@ -8,6 +8,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous" defer></script>
 </head>
+<style>
+    #my-table {
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .square {
+        width: 90px;
+        height: 100px;
+        border: 1px solid black;
+    }
+</style>
 
 <body>
     <div class="container">
@@ -18,7 +30,29 @@
         <button type="button" class="btn btn-primary">
             <a class="nav-link" href="http://127.0.0.1:8000/">Torna alla pagina Welcome</a>
         </button>
+        <div>
+            <div id="my-table"></div>
+            <button type="button" onclick="stampTable()" class="btn btn-primary">Stampa la Tabella!!</button>
+        </div>
     </div>
 </body>
 
 </html>
+
+<script>
+    const myTable = document.getElementById('my-table');
+
+    function stampTable() {
+        for (i = 0; i < 101; i++) {
+            if (i % 15 == 0) {
+                myTable.innerHTML += `<div class="square">FizzBuzz</div>`
+            } else if (i % 3 == 0) {
+                myTable.innerHTML += `<div class="square">Buzz</div>`
+            } else if (i % 5 == 0) {
+                myTable.innerHTML += `<div class="square">Fizz</div>`
+            } else {
+                myTable.innerHTML += `<div class="square">${i}</div>`
+            }
+        }
+    }
+</script>
